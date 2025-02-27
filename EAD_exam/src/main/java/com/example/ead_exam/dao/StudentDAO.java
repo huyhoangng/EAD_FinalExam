39869b6e8b1dab3b155f5c78933fe60306fba52d
huyhoangng <133932405+huyhoangng@users.noreply.github.com>
2025-02-27 10:10:39 +0700

    Add files via upload

---
+++ EAD_exam/src/main/java/com/example/ead_exam/dao/StudentDAO.java
@@ -0,0 +1,24 @@
+package com.example.ead_exam.dao;
+
+import com.example.ead_exam.entity.Student;
+import com.example.ead_exam.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class StudentDAO {
+    public static void insert(Student student) {
+        EntityManager em = JpaUtil.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(student);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public static List<Student> getAll() {
+        EntityManager em = JpaUtil.getEntityManager();
+        List<Student> students = em.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+        em.close();
+        return students;
+    }
+}
